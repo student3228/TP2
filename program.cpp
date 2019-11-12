c9e08c3b75797268e48853f31fe864bfe9d9848e
Egor Grebenkov <student3228@gmail.com>
2019-11-12 16:19:48 +0300

    Snapshot2, program.cpp, funcs.h, funcs.cpp

--- program.cpp
+++ program.cpp
@@ -3,16 +3,6 @@
 #include <iostream> 
 using namespace std;
 
-double MySum(double a, double b) 
-{
- return a+b;
-}
- 
-double MySub(double a, double b) 
-{
-return a - b;
-}
-
 int main() 
 { 
 	cout << MySum(10, 3) << endl;
